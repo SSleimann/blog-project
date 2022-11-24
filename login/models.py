84aa4fc7ec8fa367f2c19f0090ecd46a2f3d750c
Sleiman Orocua <sleimanjose23@gmail.com>
2022-11-24 15:51:13 -0400

    fix register

--- login/models.py
+++ login/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Usuario(models.Model):
     """Model definition for Usuario."""
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='usuario')
     
     def __str__(self):
         """Unicode representation of Usuario."""
