ef6cada42eee3109c4deada99d1f12d054076105
Jorge <jorgelandaeta33@gmail.com>
2023-01-30 15:56:30 -0400

    add FrontEnd part 1

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -1,10 +1,14 @@
 from django.forms import ModelForm
-
+from django import forms
 from apps.core.models import Post
 
 class CreatePostForm(ModelForm):
     """Form definition for CreatePost.
         Form for create a post"""
+
+    title = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Título de la publicación'})) 
+
+    content = forms.CharField(required=True, widget=forms.Textarea(attrs={'placeholder': '¡Deja fluir pensamientos aquí!'})) 
 
     class Meta:
         """Meta definition for CreatePostform."""
--- apps/users/forms.py
+++ apps/users/forms.py
@@ -4,22 +4,49 @@ from django.contrib.auth import get_user_model
 
 class RegisterForm(forms.ModelForm):
     """Form definition for Register."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        icons = getattr(self.Meta, 'icons', dict())
+            
+        for field_name, field in self.fields.items():
+                if field_name in icons:
+                    field.icon = icons[field_name]
+
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Nombre',
+                                                                              'autofocus': True})) 
+
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Apellido'}))
+
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Nombre de usuario'}))
+
+    email = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Correo'}))
+
     password1 = forms.CharField(label=_('Password'), 
                                 strip=False, 
                                 required=True,
-                                widget=forms.PasswordInput
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Contraseña'})
+                                        
     )
     
     password2 = forms.CharField(label=_('Password confirmation'), 
                                 strip=False, 
                                 required=True,
-                                widget=forms.PasswordInput
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Confirmar contraseña'})
     )
-    
+        
     class Meta:
         """Meta definition for Registerform."""
         model = get_user_model()
-        fields = ('email', 'username', 'first_name', 'last_name', 'bio', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'bio')
+        icons = {
+            'first_name': 'user-detail',
+            'last_name': 'user-detail',
+            'username': 'user',
+            'email': 'envelope',
+            'password1': 'lock',
+            'password2': 'lock'
+        }
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
@@ -28,7 +55,7 @@ class RegisterForm(forms.ModelForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError(_("Passwords don't match"))
         
-        return password2
+        return password1
     
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -38,14 +65,27 @@ class RegisterForm(forms.ModelForm):
         return user
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(widget=forms.TextInput(attrs={"autofocus": True}), required=True)
+    email = forms.EmailField(label='', widget=forms.TextInput(attrs={"autofocus": True, "placeholder": "Correo"}), required=True)
     
     password = forms.CharField(
-        label=_("Password"),
+        label="",
         required=True,
         strip=False,
-        widget=forms.PasswordInput(attrs={"autocomplete": "current-password"}),
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password", "placeholder": "Contraseña"}),
     )
     
-    remember_me = forms.BooleanField(label=_('Remember me'), required=False)
+    remember_me = forms.BooleanField(label=_('Recordarme'), required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        icons = getattr(self.Meta, 'icons', dict())
+
+        for field_name, field in self.fields.items():
+            if field_name in icons:
+                field.icon = icons[field_name]
     
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'password', 'remember_me']
+        icons = {'email': 'envelope',
+                 'password': 'lock-alt'}
